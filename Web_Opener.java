e04c390ad0177f485391d2e60b92f5de20303ab4
James <jfriggs1877@gmail.com>
2022-10-31 17:37:12 -0400

    added config file and this is version 1.0 of the Web_Opener working :)

--- Web_Opener.java
+++ Web_Opener.java
@@ -1,24 +1,49 @@
 
 import java.io.IOException;
 import java.util.*;
+
+import java.io.FileInputStream;
+
 class Web_Opener{
   public static void main(String args [])
   {
-    Runtime rt = Runtime.getRuntime();
+
+    Runtime run = Runtime.getRuntime();
+
+    Properties prop = new Properties();
     
-    String url;
+    String fileName = "url.config";
+    String configPropertyName = "url";
+
+    int urlNumber = 1;
+
+
     
-    Scanner sc = new Scanner(System.in);
-    
-    System.out.print(" = ");
-    url=sc.nextLine();
-    
-    try{
-      rt.exec("rundll32 url.dll,FileProtocolHandler " + url);
+try (FileInputStream fis = new FileInputStream(fileName)) {
+    prop.load(fis);
     }
-    
-    catch (IOException e) {
-      e.printStackTrace();
+
+catch (IOException ex){
+  System.out.println("error");
+}
+
+
+
+while (prop.getProperty(configPropertyName + urlNumber) != null){
+
+  try{
+    run.exec("rundll32 url.dll,FileProtocolHandler " + prop.getProperty(configPropertyName + urlNumber));
+  }
+
+  catch (IOException ex){
+    System.out.println("error");
+  }
+
+  System.out.println(prop.getProperty(configPropertyName + urlNumber));
+  urlNumber++;
+
     }
+
+
   }
 }
