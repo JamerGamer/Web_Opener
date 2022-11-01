cb59598a8ea7159d7822957df3f63310c96cf775
James <88066875+JamerGamer@users.noreply.github.com>
2022-11-01 18:59:27 -0400

    maybe work?

--- Web_Opener.java
+++ Web_Opener.java
@@ -1,29 +1,49 @@
-
 import java.io.IOException;
 import java.util.*;
-
 import java.io.FileInputStream;
 
-class Web_Opener{
-  public static void main(String args [])
-  {
-    Runtime run = Runtime.getRuntime();
-
-    Properties prop = new Properties();
-    
-    String fileName = "url.config";
-    String configPropertyName = "url";
-
-    int urlNumber = 1;
-
-    openFile(fileName);
-    openURL(configPropertyName, urlNumber);
-
-  }
 
 
+class nameNum{
+  String propertyName;
+  int urlNumber;
+}
 
-  public void openFile(String fileName){
+  class Web_Opener{
+
+    public static void main(String args []){
+      Runtime run = Runtime.getRuntime();
+
+      Properties prop = new Properties();
+      
+      String fileName = "url.config";
+      String configPropertyName = "url";
+
+      int urlNumber = 1;
+
+      openFile(fileName);
+      openURL(configPropertyName, urlNumber);
+    }
+
+    public nameNum openURL(String configPropertyName, int urlNumber){
+
+      while (prop.getProperty(configPropertyName + urlNumber) != null){
+
+        try{
+          run.exec("rundll32 url.dll,FileProtocolHandler " + prop.getProperty(configPropertyName + urlNumber));
+        }
+
+        catch (IOException ex){
+          System.out.println("error");
+        }
+
+        System.out.println(prop.getProperty(configPropertyName + urlNumber));
+        urlNumber++;
+
+    }
+
+
+    public void openFile(String fileName){
 
     try (FileInputStream fis = new FileInputStream(fileName)) {
       prop.load(fis);
@@ -34,26 +54,6 @@ class Web_Opener{
     } 
 
   }
-
-
-
-  public void openURL(String configPropertyName, int urlNumber){
-
-    while (prop.getProperty(configPropertyName + urlNumber) != null){
-
-      try{
-        run.exec("rundll32 url.dll,FileProtocolHandler " + prop.getProperty(configPropertyName + urlNumber));
-      }
-
-      catch (IOException ex){
-        System.out.println("error");
-      }
-
-      System.out.println(prop.getProperty(configPropertyName + urlNumber));
-      urlNumber++;
-
-    }
  
   }
-
-}
+  }
